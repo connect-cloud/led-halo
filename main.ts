54f6fd079fc06d119f7684988151ad4a4763767b
Bart Nijssen <73212177+connect-cloud@users.noreply.github.com>
2022-11-30 19:06:01 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 let haloDisplay = kitronik_halo_hd.createZIPHaloDisplay(60)
 basic.forever(function () {
-    haloDisplay.setBrightness(31)
+    haloDisplay.setBrightness(255)
     for (let led2 = 0; led2 <= 60; led2++) {
         haloDisplay.setZipLedColor(led2, kitronik_halo_hd.colors(ZipLedColors.Red))
         haloDisplay.show()
-        basic.pause(500)
+        basic.pause(100)
         haloDisplay.setZipLedColor(led2 - 0, kitronik_halo_hd.colors(ZipLedColors.Black))
         haloDisplay.show()
     }
+    haloDisplay.setBrightness(50)
     haloDisplay.showRainbow(1, 360)
+    basic.pause(5000)
+    haloDisplay.clear()
 })
